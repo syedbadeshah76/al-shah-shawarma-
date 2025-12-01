07f343cad2971ade46aefec6c32292113cff4f09
syedbadeshah76 <151610853+syedbadeshah76@users.noreply.github.com>
2025-12-01 16:42:11 +0530

    added something

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/al-shah-shawarma-/",
+  base:"/al-shah-shawarma-",
   server: {
     host: "::",
     port: 8080,
