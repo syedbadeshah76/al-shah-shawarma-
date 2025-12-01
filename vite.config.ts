adc96397444988aa9d37bac5bea4dbe43429f97c
syedbadeshah76 <151610853+syedbadeshah76@users.noreply.github.com>
2025-12-01 17:18:31 +0530

    addedd

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/al-shah-shawarma-",
+  base:"/al-shah-shawarma-/",
   server: {
     host: "::",
     port: 8080,
+   
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
